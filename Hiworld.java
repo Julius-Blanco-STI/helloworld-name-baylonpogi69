df8a19962b379309062848ca340347f8b685e72e
BaylonJames <58575676+BaylonJames@users.noreply.github.com>
2019-12-06 08:34:45 +0800

    Add files via upload

---
+++ Hiworld.java
@@ -0,0 +1,13 @@
+
+package hiworld;
+
+public class Hiworld {
+
+
+    public static void main(String[] args) {
+        String pogi = "Baylon";
+        System.out.println("Hello World" + pogi);
+        
+    }
+    
+}
